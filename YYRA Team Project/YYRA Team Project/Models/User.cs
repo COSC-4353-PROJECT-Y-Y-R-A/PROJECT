cbdd7d34466b6a51b0479d4c0f58df293b21c795
alonsomunoz <60762994+alonsomunoz@users.noreply.github.com>
2020-06-20 13:20:32 -0500

    Added Register page
    - Added the register page - after successful registration, will go back to login page to login
    - Added link to navbar for easy access for now, will be removed later
    - Fixed typo of navbar link for login page
    - Modified User file to have the things needed for validation (Validation requirements may change)

--- YYRA Team Project/YYRA Team Project/Models/User.cs
+++ YYRA Team Project/YYRA Team Project/Models/User.cs
@@ -5,16 +5,26 @@ namespace YYRA_Team_Project.Models
 {
     public class User
     {
+        // User for regex expressions - https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+        private const string username_error_mesage = "The Username must be longer than 8 characters, must have one letter and one number.";
+        private const string password_error_message = "The Password must be longer than 8 characters, have one uppercase letter, one lowercase letter, one number, and one special character. Special characters include (@,$,!,%,*,?,&)";
+        private const string email_error_message = "The email address must be a valid email address.";
         [Key]
-        [Display(Name = "ID")]
+        [Display(Name = "U_ID")]
         public int U_ID { get; set; }
 
-        [Required]
+        [Display(Name = "Username")]
+        [Required, MinLength(8, ErrorMessage = username_error_mesage), RegularExpression("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", ErrorMessage = username_error_mesage)]
+        public string U_Username { get; set; }
+
         [Display(Name = "Password")]
+        [Required, MinLength(8, ErrorMessage = password_error_message), RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = password_error_message)]
         public string U_Pass { get; set; }
+
 
         [Display(Name = "First name")]
         public string U_FName { get; set; }
+
 
         [Display(Name = "Last name")]
         public string U_LName { get; set; }
@@ -31,10 +41,14 @@ namespace YYRA_Team_Project.Models
         [Display(Name = "State")]
         public string U_State { get; set; }
 
-        [Display(Name = "ZipCode")]
+        [Display(Name = "Zip code")]
         public string U_Zipcode { get; set; }
 
+        [Display(Name = "Email")]
         [Required]
+        [RegularExpression("([a-zA-Z0-9])*@([a-zA-Z])*((\\.com)|(\\.net))", ErrorMessage =email_error_message)]
+        public string U_Email { get; set; }
+
         [Display(Name = "Role")]
         public string U_Role { get; set; } //admin or client
 
