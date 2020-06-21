338f22c28e9b353263025f49b7f4a5eef610fc5b
alonsomunoz <60762994+alonsomunoz@users.noreply.github.com>
2020-06-21 16:59:06 -0500

    Modified User File
    - Fixed some validation error messages, and added a few more validation requirements

--- YYRA Team Project/YYRA Team Project/Models/User.cs
+++ YYRA Team Project/YYRA Team Project/Models/User.cs
@@ -10,8 +10,9 @@ namespace YYRA_Team_Project.Models
         private const string password_error_message = "The Password must be longer than 8 characters, have one uppercase letter, one lowercase letter, one number, and one special character. Special characters include (@,$,!,%,*,?,&)";
         private const string email_error_message = "The email address must be a valid email address.";
         private const string address1_error_message = "The addresses length must be between 1 and 100 characters.";
-        private const string city_error_message = "The city length must be between 1 and 100 characters.";
+        private const string city_error_message = "The city length must equal to or less than 100 characters and all letters.";
         private const string zipcode_error_message = "The zipcode length must be between 5 and 9 characters and all numbers.";
+        private const string name_error_message = "The name length must equal to or less than 50 characters and all letters.";
         [Key]
         [Display(Name = "U_ID")]
         public int U_ID { get; set; }
@@ -33,28 +34,33 @@ namespace YYRA_Team_Project.Models
 
         [Display(Name = "First name")]
         [Required]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = name_error_message)]
         public string U_FName { get; set; }
 
 
         [Display(Name = "Last name")]
         [Required]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = name_error_message)]
         public string U_LName { get; set; }
 
         [Display(Name = "Address 1")]
         [Required]
         [MinLength(1, ErrorMessage = address1_error_message)]
         [MaxLength(100, ErrorMessage = address1_error_message)]
+        [RegularExpression("^[a-zA-Z0-9_ ]*$", ErrorMessage = address1_error_message)]
         public string U_Address1 { get; set; }
 
         [Display(Name = "Address 2")]
         [MinLength(1, ErrorMessage = address1_error_message)]
         [MaxLength(100, ErrorMessage = address1_error_message)]
+        [RegularExpression("^[a-zA-Z0-9_ ]*$", ErrorMessage = address1_error_message)]
         public string U_Address2 { get; set; }
 
         [Display(Name = "City")]
         [Required]
         [MinLength(1, ErrorMessage = city_error_message)]
         [MaxLength(100, ErrorMessage = city_error_message)]
+        [RegularExpression("^[a-zA-Z_]*$", ErrorMessage = city_error_message)]
         public string U_City { get; set; }
 
         [Display(Name = "State")]
@@ -65,7 +71,7 @@ namespace YYRA_Team_Project.Models
         [Required]
         [MinLength(5, ErrorMessage = zipcode_error_message)]
         [MaxLength(9, ErrorMessage = zipcode_error_message)]
-        [RegularExpression("^[0-9]{5,9}$")]
+        [RegularExpression("^[0-9]{5,9}$", ErrorMessage = zipcode_error_message)]
         public string U_Zipcode { get; set; }
 
         [Display(Name = "Email")]
