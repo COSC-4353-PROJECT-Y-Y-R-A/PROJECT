a8d1b490cead1bd760d94523a859d6d1c37a0152
BerryGoodz <mikyjo2012@gmail.com>
2020-06-26 09:27:40 -0500

    Added PricingModule class under Users folder

---
+++ YYRA Team Project/YYRA Team Project/Pages/Users/PricingModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YYRA_Team_Project.Pages.Users
+{
+    public class PricingModule
+    {
+        public string Name { get; set; }
+        public float Cost { get; set; }
+        public string DeliveryType { get; set; }
+        public float Discount { get; set; }
+
+        List<string> Features = new List<string>();
+        PricingModule(string n, float c)
+        {
+            Name = n;
+            Cost = c;
+        }
+        ~PricingModule()
+        {
+
+        }
+        public float CalculatePrice(float x, float y)
+        {
+            Cost = x * y;
+            return Cost;
+        }
+        public float AddTax(float c)
+        {
+            float NewCost = Cost * c;
+            return NewCost;
+        }
+        public void AddFeature(string s)
+        {
+            Features.Add(s);
+        }
+        
+    }
+
+}
