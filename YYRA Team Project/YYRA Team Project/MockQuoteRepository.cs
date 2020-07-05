630be9164bb9a357b30c6b72f399873279a68825
catpunnch <66294275+catpunnch@users.noreply.github.com>
2020-07-05 10:15:05 -0500

    fixed missing paramter, deleted extra link
    fixed missing gallon parameter in mock list, deleted extra fuel quote history link

--- YYRA Team Project/YYRA Team Project/MockQuoteRepository.cs
+++ YYRA Team Project/YYRA Team Project/MockQuoteRepository.cs
@@ -15,10 +15,11 @@ namespace YYRA_Team_Project
         {
             _quoteList = new List<Quote>()
             {
-            new Quote() { UserID = 1, Q_Address = "1 User Street", Q_Date = "Aug, 1, 2020", Q_Price = 1, Q_Total = 1},
-            new Quote() { UserID = 2, Q_Address = "2 User Street", Q_Date = "Aug, 2, 2020", Q_Price = 2, Q_Total = 2},
-            new Quote() { UserID = 3, Q_Address = "3 User Street", Q_Date = "Aug, 3, 2020", Q_Price = 3, Q_Total = 3},
-            new Quote() { UserID = 4, Q_Address = "4 User Street", Q_Date = "Aug, 4, 2020", Q_Price = 4, Q_Total = 4},
+            new Quote() { UserID = 1, Q_Gallons = 1, Q_Address = "1 User Street", Q_Date = "Aug, 1, 2020", Q_Price = 1, Q_Total = 1},
+            new Quote() { UserID = 1, Q_Gallons = 11, Q_Address = "1 User Street", Q_Date = "Aug, 11, 2020", Q_Price = 11, Q_Total = 11},
+            new Quote() { UserID = 2, Q_Gallons = 2, Q_Address = "2 User Street", Q_Date = "Aug, 2, 2020", Q_Price = 2, Q_Total = 2},
+            new Quote() { UserID = 3, Q_Gallons = 3, Q_Address = "3 User Street", Q_Date = "Aug, 3, 2020", Q_Price = 3, Q_Total = 3},
+            new Quote() { UserID = 4, Q_Gallons = 4, Q_Address = "4 User Street", Q_Date = "Aug, 4, 2020", Q_Price = 4, Q_Total = 4},
             };
 
         }
