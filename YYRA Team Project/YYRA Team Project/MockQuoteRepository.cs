90e48edb3103fcc9219a734b44a8055c3eb610db
catpunnch <66294275+catpunnch@users.noreply.github.com>
2020-07-05 19:47:44 -0500

    Added more fake quotes
    Addd more fake quotes, increased all the values in the quotes +1

--- YYRA Team Project/YYRA Team Project/MockQuoteRepository.cs
+++ YYRA Team Project/YYRA Team Project/MockQuoteRepository.cs
@@ -15,11 +15,20 @@ namespace YYRA_Team_Project
         {
             _quoteList = new List<Quote>()
             {
-            new Quote() { UserID = 1, Q_Gallons = 1, Q_Address = "1 User Street", Q_Date = "Aug, 1, 2020", Q_Price = 1, Q_Total = 1},
-            new Quote() { UserID = 1, Q_Gallons = 11, Q_Address = "1 User Street", Q_Date = "Aug, 11, 2020", Q_Price = 11, Q_Total = 11},
-            new Quote() { UserID = 2, Q_Gallons = 2, Q_Address = "2 User Street", Q_Date = "Aug, 2, 2020", Q_Price = 2, Q_Total = 2},
-            new Quote() { UserID = 3, Q_Gallons = 3, Q_Address = "3 User Street", Q_Date = "Aug, 3, 2020", Q_Price = 3, Q_Total = 3},
-            new Quote() { UserID = 4, Q_Gallons = 4, Q_Address = "4 User Street", Q_Date = "Aug, 4, 2020", Q_Price = 4, Q_Total = 4},
+            new Quote() { UserID = 2, Q_Gallons = 2, Q_Address = "2 User Street", Q_Date = "Feb, 2, 2020", Q_Price = 2, Q_Total = 2},
+
+            new Quote() { UserID = 2, Q_Gallons = 22, Q_Address = "2 User Street", Q_Date = "Feb, 22, 2020", Q_Price = 22, Q_Total = 11},
+
+            new Quote() { UserID = 3, Q_Gallons = 3, Q_Address = "3 User Street", Q_Date = "Mar, 3, 2020", Q_Price = 3, Q_Total = 3},
+
+            new Quote() { UserID = 3, Q_Gallons = 33, Q_Address = "3 User Street", Q_Date = "Mar, 3, 2020", Q_Price = 33, Q_Total = 33},
+
+            new Quote() { UserID = 4, Q_Gallons = 4, Q_Address = "4 User Street", Q_Date = "April, 4, 2020", Q_Price = 4, Q_Total = 4},
+            new Quote() { UserID = 4, Q_Gallons = 44, Q_Address = "4 User Street", Q_Date = "April, 4, 2020", Q_Price = 44, Q_Total = 44},
+            new Quote() { UserID = 4, Q_Gallons = 444, Q_Address = "4 User Street", Q_Date = "April, 4, 2020", Q_Price = 444, Q_Total =444},
+
+            new Quote() { UserID = 5, Q_Gallons = 5, Q_Address = "5 User Street", Q_Date = "May, 5, 2020", Q_Price = 5, Q_Total = 5},
+            new Quote() { UserID = 5, Q_Gallons = 55, Q_Address = "5 User Street", Q_Date = "May, 5, 2020", Q_Price = 55, Q_Total = 55},
             };
 
         }
