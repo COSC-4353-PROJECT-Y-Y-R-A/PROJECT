000c2655739b4e23ed52d1b66d3c3deddb0b8157
catpunnch <66294275+catpunnch@users.noreply.github.com>
2020-07-05 19:01:47 -0500

    comparison not working

--- YYRA Team Project/YYRA Team Project/Pages/Users/UserQuotes.cshtml.cs
+++ YYRA Team Project/YYRA Team Project/Pages/Users/UserQuotes.cshtml.cs
@@ -13,9 +13,9 @@ namespace YYRA_Team_Project.Pages.Users
     public class UserQuotesModel : PageModel
     {
         private readonly IQuoteRepository quoteRepository;
-
-        //all quotes
         public IEnumerable<Quote> Quotes { get; set; }
+
+        public int CurrentId { get; set; }
 
         public UserQuotesModel(IQuoteRepository quoteRepository)
         {
@@ -28,43 +28,25 @@ namespace YYRA_Team_Project.Pages.Users
         //all quotes
         public IQuoteRepository QuoteRepository { get; }
 
+        /*
         // Model-binding automatically maps the query string id
         // value to the id parameter on this OnGet() method
         public void OnGet(int id)
         {
             Quote = quoteRepository.GetQuote(id);
             Quotes = quoteRepository.GetAllQuotes();
-        }
+        }*/
 
         //get ID of user logged in
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public void OnGetAsync(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            if (HttpContext.Session.Get("Username") != null)
-            {
-                byte[] str = HttpContext.Session.Get("Username");
-                string Username = Encoding.UTF8.GetString(str, 0, str.Length);
-                ViewData["Username"] = Username;
-            }
-
-            if (HttpContext.Session.Get("Role") != null)
-            {
-                byte[] str = HttpContext.Session.Get("Role");
-                string Role = Encoding.UTF8.GetString(str, 0, str.Length);
-                ViewData["Role"] = Role;
-            }
             if (HttpContext.Session.Get("Id") != null)
             {
                 byte[] str = HttpContext.Session.Get("Id");
                 string ID = Encoding.UTF8.GetString(str, 0, str.Length);
                 ViewData["Id"] = ID;
-            }
-          
-            return Page();
+           
         }
+
     }
 }
