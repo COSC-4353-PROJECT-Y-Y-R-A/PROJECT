10b289312dd59c9a1b14279aa49be950bace5833
catpunnch <66294275+catpunnch@users.noreply.github.com>
2020-07-05 19:28:56 -0500

    need to fix User Quote condition
    need to extract user ID of logged in user and store it as an int.

--- YYRA Team Project/YYRA Team Project/Pages/Users/UserQuotes.cshtml.cs
+++ YYRA Team Project/YYRA Team Project/Pages/Users/UserQuotes.cshtml.cs
@@ -15,8 +15,6 @@ namespace YYRA_Team_Project.Pages.Users
         private readonly IQuoteRepository quoteRepository;
         public IEnumerable<Quote> Quotes { get; set; }
 
-        public int CurrentId { get; set; }
-
         public UserQuotesModel(IQuoteRepository quoteRepository)
         {
             this.quoteRepository = quoteRepository;
@@ -28,25 +26,16 @@ namespace YYRA_Team_Project.Pages.Users
         //all quotes
         public IQuoteRepository QuoteRepository { get; }
 
-        /*
+        
         // Model-binding automatically maps the query string id
         // value to the id parameter on this OnGet() method
         public void OnGet(int id)
         {
             Quote = quoteRepository.GetQuote(id);
             Quotes = quoteRepository.GetAllQuotes();
-        }*/
-
-        //get ID of user logged in
-        public void OnGetAsync(int? id)
-        {
-            if (HttpContext.Session.Get("Id") != null)
-            {
-                byte[] str = HttpContext.Session.Get("Id");
-                string ID = Encoding.UTF8.GetString(str, 0, str.Length);
-                ViewData["Id"] = ID;
-           
         }
+
+      
 
     }
 }
