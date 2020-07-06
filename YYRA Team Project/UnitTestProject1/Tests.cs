73745b4c82a51c3f9dc423efc6b2043992cadbbf
Robert Hines <47727369+RobertHines45@users.noreply.github.com>
2020-07-05 23:11:50 -0500

    UserLogin unit tests

--- YYRA Team Project/UnitTestProject1/Tests.cs
+++ YYRA Team Project/UnitTestProject1/Tests.cs
@@ -3,6 +3,8 @@ using YYRA_Team_Project.Pages.Users;
 using YYRA_Team_Project.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
+using System;
+using YYRA_Team_Project;
 
 namespace Project_Tests
 {
@@ -21,8 +23,56 @@ namespace Project_Tests
             var model = new UserProfileModel();
             var res = model.OnGetAsync(id);
 
-            Assert.IsNull(model.USERS);
+            //Assert.IsNull(model.USERS);
             Assert.IsNotNull(model.USERS);
         }
+
+        [TestMethod]
+        public void UserLogin_OnGet_Test() //tests if the onget functionality 
+        {
+            int id = 2;
+            var TestUser = new User() { U_ID = id, U_Username = "userAccount1", U_Pass = "passworD1!" };
+            var mul = new MockUserList();
+            mul.setUser(TestUser);
+
+            var model = new UserLoginModel();
+
+
+            Assert.ThrowsException<InvalidOperationException>(
+              () =>  model.OnGetAsync(id)
+            );
+        }
+
+        [TestMethod]
+        public void UserLogin_OnPost_Admin_Test()
+        { //tests if user of admin status makes it through on post
+            int id = 2;
+            var TestUser = new User() { U_ID = id, U_Username = "userAccount1", U_Pass = "passworD1!", U_Role = "Admin" };
+            var mul = new MockUserList();
+            mul.setUser(TestUser);
+
+            var model = new UserLoginModel();
+
+            Assert.ThrowsException<InvalidOperationException>(
+              () =>  model.OnPostAsync(id)
+            );
+
+        }
+
+        [TestMethod]
+        public void UserLogin_OnPost_Client_Test()
+        { //tests if user of client status makes it through on post
+            int id = 2;
+            var TestUser = new User() { U_ID = id, U_Username = "userAccount1", U_Pass = "passworD1!", U_Role = "Client" };
+            var mul = new MockUserList();
+            mul.setUser(TestUser);
+
+            var model = new UserLoginModel();
+
+            Assert.ThrowsException<InvalidOperationException>(
+              () => model.OnPostAsync(id)
+            );
+        }
+
     }
 }
