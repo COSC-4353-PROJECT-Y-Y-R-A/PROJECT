a46eec36c65ffec198057477c948e13b63b83a4e
catpunnch <66294275+catpunnch@users.noreply.github.com>
2020-07-12 07:21:17 -0500

    admin quote db works
    admin quote db works, only added 1 mock quote for now. will add more later.

--- YYRA Team Project/YYRA Team Project/Pages/Users/QuoteHistoryDisplay.cshtml.cs
+++ YYRA Team Project/YYRA Team Project/Pages/Users/QuoteHistoryDisplay.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using YYRA_Team_Project.Models;
 
+
 namespace YYRA_Team_Project.Pages.Users
 {
     public class QuoteHistoryDisplayModel : PageModel
