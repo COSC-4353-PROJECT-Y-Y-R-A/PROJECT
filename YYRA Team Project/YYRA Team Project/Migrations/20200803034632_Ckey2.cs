5ae207d92ae5f4541b6d34cdbd8f0f052f49dba1
catpunnch <66294275+catpunnch@users.noreply.github.com>
2020-08-02 22:53:59 -0500

    new migration for composite key

---
+++ YYRA Team Project/YYRA Team Project/Migrations/20200803034632_Ckey2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YYRA_Team_Project.Migrations
+{
+    public partial class Ckey2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
