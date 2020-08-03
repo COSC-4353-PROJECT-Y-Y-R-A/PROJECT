be09ac4fa52dd06570d0d6e2fe6c66d646e20540
catpunnch <66294275+catpunnch@users.noreply.github.com>
2020-08-03 03:59:05 -0500

    deleted old tables and recreated
    merge conflict bc of existing tables, couldnt update dB and couldnt add new quote table.

--- YYRA Team Project/YYRA Team Project/Migrations/20200803085057_Initial.Designer.cs
+++ YYRA Team Project/YYRA Team Project/Migrations/20200803085057_Initial.Designer.cs
@@ -9,28 +9,72 @@ using YYRA_Team_Project.Data;
 namespace YYRA_Team_Project.Migrations
 {
     [DbContext(typeof(YYRA_Team_ProjectContext))]
-    [Migration("20200712101919_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20200803085057_Initial")]
+    partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.5")
+                .HasAnnotation("ProductVersion", "3.1.6")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("YYRA_Team_Project.Models.Quote", b =>
+            modelBuilder.Entity("YYRA_Team_Project.Models.ClientInfo", b =>
                 {
-                    b.Property<int>("UserID")
+                    b.Property<int>("C_ID")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("P_Name")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(50)")
+                        .HasMaxLength(50);
+
+                    b.Property<string>("U_Address1")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(100)")
+                        .HasMaxLength(100);
+
+                    b.Property<string>("U_Address2")
+                        .HasColumnType("nvarchar(100)")
+                        .HasMaxLength(100);
+
+                    b.Property<string>("U_City")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(100)")
+                        .HasMaxLength(100);
+
+                    b.Property<int>("U_ID")
+                        .HasColumnType("int");
+
+                    b.Property<string>("U_State")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("U_Zipcode")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(9)")
+                        .HasMaxLength(9);
+
+                    b.HasKey("C_ID");
+
+                    b.ToTable("ClientInformation");
+                });
+
+            modelBuilder.Entity("YYRA_Team_Project.Models.Quote", b =>
+                {
+                    b.Property<int>("QID")
+                        .HasColumnType("int");
+
+                    b.Property<int>("UserID")
+                        .HasColumnType("int");
 
                     b.Property<string>("Q_Address")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Q_Date")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<int>("Q_Gallons")
@@ -42,7 +86,7 @@ namespace YYRA_Team_Project.Migrations
                     b.Property<float>("Q_Total")
                         .HasColumnType("real");
 
-                    b.HasKey("UserID");
+                    b.HasKey("QID", "UserID");
 
                     b.ToTable("Quote");
                 });
@@ -75,7 +119,8 @@ namespace YYRA_Team_Project.Migrations
 
                     b.Property<string>("U_Pass")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                        .HasColumnType("nvarchar(16)")
+                        .HasMaxLength(16);
 
                     b.Property<string>("U_Role")
                         .IsRequired()
@@ -86,18 +131,42 @@ namespace YYRA_Team_Project.Migrations
 
                     b.Property<string>("U_Username")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                        .HasColumnType("nvarchar(16)")
+                        .HasMaxLength(16);
 
                     b.Property<string>("U_Zipcode")
                         .HasColumnType("nvarchar(9)")
                         .HasMaxLength(9);
 
-                    b.Property<int?>("userRole")
-                        .HasColumnType("int");
-
                     b.HasKey("U_ID");
 
                     b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("YYRA_Team_Project.Models.UserCred", b =>
+                {
+                    b.Property<int>("U_ID")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int")
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("U_Pass")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(16)")
+                        .HasMaxLength(16);
+
+                    b.Property<string>("U_Role")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("U_Username")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(16)")
+                        .HasMaxLength(16);
+
+                    b.HasKey("U_ID");
+
+                    b.ToTable("UserCredentials");
                 });
 #pragma warning restore 612, 618
         }
